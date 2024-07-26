5bec45c38649552bad02e50b45d700bd4bae5534
qrapidnow <qrapidnow@gmail.com>
2024-07-27 02:00:30 +0530

    firebase deploy

--- qrapid-dashboard/vite.config.js
+++ qrapid-dashboard/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Ensure the output directory is correctly set
   },
 });

ba5c769196cef6da490acf8b5d909fbc1ccdc39d
qrapidnow <qrapidnow@gmail.com>
2024-07-27 16:54:06 +0530

    fix spacing

--- qrapid-dashboard/src/firebase-config.jsx
+++ qrapid-dashboard/src/firebase-config.jsx
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_EztMkylp3MDgSAaayedXLNO106NicY8",
-    authDomain: "qr-dashboard-1107.firebaseapp.com",
-    projectId: "qr-dashboard-1107",
-    storageBucket: "qr-dashboard-1107.appspot.com",
-    messagingSenderId: "625009659509",
-    appId: "1:625009659509:web:155c88e41039dce38ab23c"
-  };
+  apiKey: "AIzaSyB_EztMkylp3MDgSAaayedXLNO106NicY8",
+  authDomain: "qr-dashboard-1107.firebaseapp.com",
+  projectId: "qr-dashboard-1107",
+  storageBucket: "qr-dashboard-1107.appspot.com",
+  messagingSenderId: "625009659509",
+  appId: "1:625009659509:web:155c88e41039dce38ab23c"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

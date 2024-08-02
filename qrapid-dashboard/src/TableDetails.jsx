e1f0dc3fa2c0f2ce69e1c7178f25b92f3572c98e
qrapidnow <qrapidnow@gmail.com>
2024-08-02 08:23:06 +0530

    new changes

--- qrapid-dashboard/src/TableDetails.jsx
+++ qrapid-dashboard/src/TableDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { backendDb, frontendDb as db } from './firebase-config'; // Import frontendDb and rename it to db
+import { backendDb, db } from './firebase-config'; // Import frontendDb as db
 import { collection, query, where, onSnapshot, doc, getDoc, orderBy, getDocs, writeBatch } from 'firebase/firestore';
 import './TableDetails.css';
 
